8952290e88cb0290e6a5a762e52cc26b9654f298
JP Bulman <jpbulman@wpi.edu>
2020-05-19 22:15:35 -0400

    Updated counter and resource rendering to be API based

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -14,28 +14,29 @@ export const heightOfSVG = Number(
   document.getElementById("root")?.offsetHeight
 );
 
-// Stolen from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
-function shuffle(a: Array<any>) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
+export interface BoardState {
+  resources: Array<string>;
+  counters: Array<number>;
+}
+
+export class Board extends React.Component<{}, BoardState> {
+  constructor() {
+    super({});
+    this.state = {
+      resources: [],
+      counters: [],
+    };
   }
-  return a;
-}
 
-export interface BoardProps {
-  Tiles?: Array<Tile>;
-}
-
-export class Board extends React.Component<{}, {}> {
   makeTiles() {
     let tileKey = 0;
     const widthOfSVG = Number(document.getElementById("root")?.offsetWidth);
     const heightOfSVG = Number(document.getElementById("root")?.offsetHeight);
 
     let arrTiles = [];
-    const resources = this.randomResourceSequence();
-    const counters = this.randomCounterSequence();
+    // const resources = this.randomResourceSequence();
+    const { resources, counters } = this.state;
+    // const counters = this.randomCounterSequence();
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < i + 3; j++) {
         //Absolute GARBAGE
@@ -53,7 +54,9 @@ export class Board extends React.Component<{}, {}> {
             resource={resources[tileKey]}
             tipX={x}
             tipY={y}
-            counter={resources[tileKey] === "desert" ? -1 : counters.pop()}
+            counter={
+              resources[tileKey] === "desert" ? -1 : Number(counters.pop())
+            }
           />
         );
         tileKey++;
@@ -68,7 +71,9 @@ export class Board extends React.Component<{}, {}> {
               resource={resources[tileKey]}
               tipX={x}
               tipY={y}
-              counter={resources[tileKey] === "desert" ? -1 : counters.pop()}
+              counter={
+                resources[tileKey] === "desert" ? -1 : Number(counters.pop())
+              }
             />
           );
           tileKey++;
@@ -78,35 +83,19 @@ export class Board extends React.Component<{}, {}> {
     return arrTiles;
   }
 
-  // Returns an array of strings representing resources put in random order. Reads left to right starting from the top leftmost tile
-  // Follows standard rules: 4 wood, 4 sheep, 3 brick, 4 wheat, 3 ore, 1 desert
-  randomResourceSequence() {
-    let resourcesSequence = ["desert"];
-    for (let i = 0; i < 4; i++) {
-      resourcesSequence = resourcesSequence
-        .concat("wood")
-        .concat("sheep")
-        .concat("wheat");
-    }
-    for (let i = 0; i < 3; i++) {
-      resourcesSequence = resourcesSequence.concat("brick").concat("ore");
-    }
-
-    return shuffle(resourcesSequence);
+  makeNewGame() {
+    fetch("http://localhost:3001/games", { method: "POST" })
+      .then((resp) => resp.json())
+      .then((res) =>
+        this.setState({
+          counters: res.counters.map((el: string) => Number(el)),
+          resources: res.resources,
+        })
+      );
   }
 
-  randomCounterSequence() {
-    let counterSequence = [2, 12];
-    for (let i = 0; i < 2; i++) {
-      for (let j = 3; j < 12; j++) {
-        if (j === 7) {
-          continue;
-        } else {
-          counterSequence.push(j);
-        }
-      }
-    }
-    return shuffle(counterSequence);
+  componentDidMount() {
+    this.makeNewGame();
   }
 
   constructPorts() {
