422bf21a0d567928f6e8e036470d7091f91a955d
JP Bulman <jpbulman@wpi.edu>
2020-05-28 10:00:21 -0400

    Added some basic entites to match the API info

---
+++ src/entities/Player.tsx
@@ -0,0 +1,13 @@
+import { Building } from "./Building";
+
+export class Player {
+  victoryPoints: number;
+  settlements: Array<Building>;
+
+  constructor() {
+    this.victoryPoints = 0;
+    this.settlements = [];
+  }
+
+  //   constructor
+}
